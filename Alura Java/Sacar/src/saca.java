2f4a84f4cb7fc0ec06db7c23468464fa982763d1
P4NDAPOWER <79761270+larissa4lves@users.noreply.github.com>
2022-05-16 14:39:11 -0300

    update

---
+++ Alura Java/Sacar/src/saca.java
@@ -0,0 +1,12 @@
+
+public boolean saca(double valor) {
+
+	    if(saldo >= valor) {
+	        saldo -= valor;
+	        return true;
+	    } else {
+	        return false;
+	    
+	        {
+	        	}
+	        }
---
+++ Alura Java/sintaxe-variaveis-e-fluxo/src/TestaVariaveis.java
@@ -0,0 +1,14 @@
+
+public class TestaVariaveis {
+	
+	public static void main(String[] args) {
+		System.out.println("sua nota é ? ");
+		
+		double nota;
+		nota = 9.8;
+		
+		System.out.println(nota);
+	}
+	
+	
+}
